59ae09f977e60743ec8cfa5f2c02a31f8addf9c9
shahzaib <shahzaibazhar98@gmail.com>
2025-05-05 17:42:14 +0500

    4rth commit new folder structure

--- api/handler.js
+++
@@ -1,6 +0,0 @@
-// api/handler.js or same index.js
-const app = require('./index');
-
-module.exports = (req, res) => {
-  return app(req, res);
-};
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+// const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from gettttttttttt Express!');
+});
+
+app.get('/data', (req, res) => {
+  res.send('Hello from gettttttttttt dataaaaaaaa!');
+});
+
+app.post('/post', (req, res) => {
+  res.send('Hello from Postttttttttt Express!');
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;
